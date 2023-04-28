c26e884fe2c2747f9e26bf43913885bfd1935aa0
adrianakatarina <dricakatarina@gmail.com>
2023-04-28 15:25:38 -0300

    Implementação do CLI

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,18 @@
-import { mdLinks } from './index.js';
-import chalk from 'chalk';
+#! /usr/bin/env node
 
-mdLinks('files/texto.md')
+import { mdLinks } from './md-links.js';
+import chalk from 'chalk';
+import { argv } from 'node:process'
+
+const caminhoDoArquivo = argv[2];
+
+mdLinks(caminhoDoArquivo)
 .then((informacoes) =>{
-    console.log(chalk.blue(informacoes[0].href));
+    informacoes.map((item) => {
+      console.log(`${chalk.magenta(item.file)} ${chalk.cyanBright(item.href)} ${chalk.yellowBright(item.text)}`);
+    })
+    
 }).catch((err)=>{
     console.log(err);
 })
+
